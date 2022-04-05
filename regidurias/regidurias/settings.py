96ae3a23d607e207bd7e14285646ecadf7221bb9
PerlaTovar <pataditaenloshuevos@gmail.com>
2022-04-05 07:35:29 -0500

    "GET /logo.png HTTP/1.1" 404 3841

--- regidurias/regidurias/settings.py
+++ regidurias/regidurias/settings.py
@@ -168,9 +168,32 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
+STATIC_URL = '/statics/'
 
-STATIC_URL = '/statics/statics/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'statics/') 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'statics/media')
-MEDIA_URL = '/statics/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'statics'),
+]
+
+# LOGIN_REDIRECT_URL = '/erp/dashboard/'
+
+# LOGOUT_REDIRECT_URL = '/login/'
+
+# LOGIN_URL = '/login/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
+
+# STATIC_URL = '/statics/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR, "statics"),]
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'statics/media')
+# MEDIA_URL = '/statics/media/'
+
+APPEND_SLASH = True
