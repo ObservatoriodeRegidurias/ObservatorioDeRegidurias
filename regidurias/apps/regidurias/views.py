ed11eec10652200ede70039742823d82274f06f2
Perla Tovar <perla@fondify.org>
2022-05-16 14:10:49 -0500

    tabla

--- regidurias/apps/regidurias/views.py
+++ regidurias/apps/regidurias/views.py
@@ -14,7 +14,6 @@ class RegiduriasView(View):
     def get(self, request, *args, **kwargs):
         regidores =  Regidurias.objects.all().exclude(status=1)
         regidores_filter = RegiduriasFilter(request.GET, queryset=regidores)
-        #return render(reverse("regidurias/template.html", kwargs={"regidurias": regidores.slug}))
         return render(request, 'regidurias/template.html', {'filter': regidores_filter})
 
 
@@ -30,12 +29,3 @@ class RegidorDetailView(DetailView):
         post = Regidurias.objects.filter(slug=self.kwargs.get('slug'))
 
         return context
-# class RegidorView(View):
-#     # @property
-#     # def regidurias(self):
-#     #     return self.kwargs.get('regidurias')
-        
-#     def get(self, request, *args, **kwargs):
-#         regidurias = Regidurias.objects.get(slug=self.regidurias)
-#         #regidurias =  Regidurias.objects.filter(slug=self.regidurias).first()
-#         return render(request, "regidurias/regidor.html", locals())
